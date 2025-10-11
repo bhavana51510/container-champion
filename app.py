2454b9ba4bf144edb7bcd5e71c8e0914f7e66c05
bhavana51510 <145677295+bhavana51510@users.noreply.github.com>
2025-10-11 23:03:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from Container Champion!"
+    return "Hi from Container Champion!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
+
