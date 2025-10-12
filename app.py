178a0e653d6e10cfa52fe5d50f0fa6351d62df61
Bhavana <bhavanamalreddy2705@gmail.com>
2025-10-12 22:46:39 +0530

    Demo trigger ci/cd

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from Container Champion!"
+    return "Hello from Container Champion!"  # just to show a change
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
